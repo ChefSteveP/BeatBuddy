a4a305cc5c0680176d95e7585147b7f14f6c4f8f
averydossman <averydossman@gmail.com>
2023-06-01 16:00:23 -0400

    Added my profile tab

--- frontend/src/Components/Login/NavBar.js
+++ frontend/src/Components/Login/NavBar.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { AppBar, Toolbar, Button } from '@mui/material';
+import { AppBar, Toolbar, Button, Box } from '@mui/material';
 
 const NavBar = () => {
   return (
     <AppBar position="static" sx={{ backgroundColor: '#1DB954' }}>
       <Toolbar>
-        <Button color="inherit" component={Link} to="/inbox">
-          Inbox
+      <Button color="inherit" component={Link} to="/discover">
+          Discover
         </Button>
         <Button color="inherit" component={Link} to="/forum">
           Forum
         </Button>
-        <Button color="inherit" component={Link} to="/discover">
-          Discover
+        <Button color="inherit" component={Link} to="/inbox">
+          Inbox
+        </Button>
+        <Box sx={{ flexGrow: 1 }} />
+        <Button color="inherit" component={Link} to="/myProfile">
+          My Profile
         </Button>
       </Toolbar>
     </AppBar>
